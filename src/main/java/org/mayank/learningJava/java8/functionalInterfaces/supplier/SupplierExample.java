50f751b26429c549ffadfe7b45c031d484b1687f
mayyaannkk <mayankpant.dev@gmail.com>
2025-08-07 09:08:10 +0530

    supplier changes

--- src/main/java/org/mayank/learningJava/java8/functionalInterfaces/supplier/SupplierExample.java
+++ src/main/java/org/mayank/learningJava/java8/functionalInterfaces/supplier/SupplierExample.java
@@ -1,14 +1,26 @@
 package org.mayank.learningJava.java8.functionalInterfaces.supplier;
 
+import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
-public class _Supplier {
+public class SupplierExample {
     public static void main(String[] args) {
         System.out.println("=== OLD ===");
         System.out.println(getDBConnection());
 
         System.out.println("=== NEW ===");
         System.out.println(getDBConnectionSupplier.get());
+
+        // Generating random values
+        /*
+        IntSupplier generateRandomInt = () -> Math.random()*10;
+        In Java, narrowing primitive conversions (like double → int) can cause data loss, so the compiler:
+
+        Allows widening (int → double)
+
+        But disallows narrowing without an explicit cast
+
+         */
     }
 
     static String getDBConnection() {
